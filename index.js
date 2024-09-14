1d392a38a93ddfdde14bd06e2c1870867c8d66e7
Bar Amit <bar.amit@outlook.com>
2024-09-14 20:48:30 +0300

    add comments & refactor

--- index.js
+++ index.js
@@ -3,56 +3,84 @@ import { Client } from "@gradio/client";
 const client = await Client.connect("guymorlan/levanti_he_ar");
 
 const api = {
-    async audio(text){
+    /**
+     * Creates an audio of an Arabic sentence.
+     * @param {String} text A diacritized Arabic text
+     * @returns An object with a path to an audio file
+     */
+    async get_audio(text){
         const result = await client.predict("/get_audio", { 		
             input_text: text, 
         });
-        console.log(result.data);
+        return result.data;
     },
-    async en2ar(){
-        const result = await client.predict("/en2ar", { 		
-            text: "ניסיון", 		
-            dialect: "פלסטיני", 
-        });
-        console.log(result.data);
-    },
-    async run_translate(text){
+    /**
+     * Translates a sentence in Hebrew into Arabic.
+     * @param {String} text A text in Hebrew.
+     * @returns An arabic translation.
+     */
+    async get_translation(text){
         const client = await Client.connect("guymorlan/levanti_he_ar");
         const result = await client.predict("/run_translate", { 		
                 text,
                 dialect: "פלסטיני",
         });
-        console.log(result.data);
         return result.data[1];
     },
-    async diacriteize(text){
+    /**
+     * Diacritizes an Arabic text.
+     * @param {String} text Arabic text.
+     * @returns Diacritized text.
+     */
+    async get_diacritize(text){
         const client = await Client.connect("guymorlan/levanti_he_ar");
         const result = await client.predict("/diacritize", { 		
                 text, 
         });
-        console.log(result.data);
         return result.data[0];
     },
-    async trans_lettering(text){
+    /**
+     * Creates a taatik (transliteration) in Hebrew of an Arabic text.
+     * @param {String} text Diacritized Arabic text.
+     * @returns Hebrew transliteration of the text
+     */
+    async get_taatik(text){
         const client = await Client.connect("guymorlan/levanti_he_ar");
         const result = await client.predict("/taatik", { 		
                 text, 
         });
-        console.log(result.data);
+        return result.data;
     },
-    async update_df(text){
+    /**
+     * Gets a list of similar sentences translated in the past.
+     * @param {String} text Arabic text.
+     * @returns A list of Arabic sentences and their Hebrew transliterations.
+     */
+    async get_similar(text){
         const result = await client.predict("/update_df", { 		
                 hidden_arabic: text, 
         });
-        
-        console.log(result.data[0].value.data);
+        return result.data[0].value.data;
+    },
+    /**
+     * A function to grub all available features in one go.
+     * @param {String} text Hebrew text for translation.
+     * @returns Data from all the functions above.
+     */
+    async get_all_data(text){
+        let translation = await api.get_translation(text);
+        let [diacriteized, suggestions] = await Promise.all([
+            this.get_diacritize(translation),
+            this.get_similar(translation),
+        ]);
+        let [audio, taatik] = await Promise.all([
+            this.get_audio(diacriteized),
+            this.get_taatik(diacriteized),
+        ]);
+        return {translation, diacriteized, suggestions, audio, taatik};
     }
 };
 
-(async function(){
-    let text = await api.run_translate('ניסיון ראשון');
-    let diacriteized = await api.diacriteize(text);
-    api.audio(diacriteized);
-    api.trans_lettering(diacriteized);
-    api.update_df(text);
-})();
+// (async function(){
+//     console.log(await api.get_all_data('אני בודק שהכל עובד חלק.'));
+// })();
